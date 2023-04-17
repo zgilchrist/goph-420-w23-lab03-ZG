66704f666a6d114d2b6f6beb6f7bdb2df91f2421
zgilchrist <72540268+zgilchrist@users.noreply.github.com>
2023-04-17 16:02:07 -0600

    plotted fifth event

--- src/driver.py
+++ src/driver.py
@@ -3,132 +3,166 @@ from regression import multi_regress
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 def main():
     """Main function"""
-    
-    #tests the regression tool based on an in-class example
-    testing_data()
 
-    #Parses data from raw data
+    # tests the regression tool based on an in-class example
+    # toggle comment to use it
+    # testing_data()
+
+    # Parses data from raw data
     data = np.loadtxt("M_data.txt")
-    hours = data[:,0]
-    magnitudes = data[:,1]
-    
-    #Determined visually from graphical plot that there are four distinct events (measured in hours)
-    events = [34,46,71,96]
+    hours = data[:, 0]
+    magnitudes = data[:, 1]
+
+    # Determined visually from graphical plot that there are four distinct events (measured in hours)
+    events = [34, 46, 71, 96]
     
     plt.figure()
     plt.xlabel("time, t[hrs]")
     plt.ylabel("event magnitude, M")
+    plt.title("Raw Earthquake Event Data")
+    plt.plot(hours, magnitudes, "b.")
+    plt.savefig("Raw Earthquake Event Data.png")
+       
+    plt.figure()
+    plt.xlabel("time, t[hrs]")
+    plt.ylabel("event magnitude, M")
     plt.title("Raw Earthquake Event Data: Red Lines Indicate an Event")
-    plt.plot(hours,magnitudes,"b.")
-    plt.vlines(events,ymin=-2,ymax=2,colors = 'red')
+    plt.plot(hours, magnitudes, "b.")
+    plt.vlines(events, ymin=-2, ymax=2, colors="red")
+    plt.savefig("Events on Raw Earthquake Event Data.png")
 
-    #Create intervals of magnitudes to create N counts with
-    M_thresh = np.array([-0.5,-0.25,0,0.25,0.5,0.75,1.0])
-    #M_thresh = np.array([-1.25,-1.0,-0.75,-0.5,-0.25,0,0.25,0.5,0.75,1.0, 1.25, 1.5, 1.75,2.])
-    
-    
-    
-    #Parses the events to find index where that event stops, then creates array of indices to be used later
+    # Create intervals of magnitudes to create N counts with
+    M_thresh = np.array([-0.5, -0.25, 0, 0.25, 0.5, 0.75, 1.0])
+    # M_thresh = np.array([-1.25,-1.0,-0.75,-0.5,-0.25,0,0.25,0.5,0.75,1.0, 1.25, 1.5, 1.75,2.])
+
+    # small events can't be detected
+    # large events not big sample size
+
+    # Parses the events to find index where that event stops, then creates array of indices to be used later
     indices = np.zeros(0)
-    j=0
+    j = 0
     for event in events:
         while event > hours[j]:
-            j+=1
-            #if j == 13390:
-                 #break
-        indices = np.append(indices,j)
-   
-   #Plot setup
+            j += 1
+            # if j == 13390:
+            # break
+        indices = np.append(indices, j)
+
+    # Plot setup
     plt.figure()
-    plt.xlabel("Magnitude")
+    plt.xlabel("magnitude")
     plt.ylabel("log(N) number of events")
     plt.title("log(N) frequency of events at M magnitude")
-    
-    
-    #Each curve plots the magnitude interval against the log_10 of the frequency of events at each interval by calling get_counts
-    n_1 = np.log10(get_counts(M_thresh,magnitudes[:int(indices[0])]))
-    plt.plot(M_thresh,n_1,"r.-", label = "Event 1")
-    
-    n_2 = np.log10(get_counts(M_thresh,magnitudes[int(indices[0]):int(indices[1])]))
-    plt.plot(M_thresh,n_2,"b.-", label = "Event 2")
 
-    n_3 = np.log10(get_counts(M_thresh,magnitudes[int(indices[1]):int(indices[2])]))
-    plt.plot(M_thresh,n_3,"g.-", label = "Event 3")
+    # Each curve plots the magnitude interval against the log_10 of the frequency of events at each interval by calling get_counts
+    n_1 = np.log10(get_counts(M_thresh, magnitudes[: int(indices[0])]))
+    plt.plot(M_thresh, n_1, "r.-", label="Event 1")
 
-    n_4 = np.log10(get_counts(M_thresh,magnitudes[int(indices[2]):int(indices[3])]))
-    plt.plot(M_thresh,n_4,"y.-", label = "Event 4")
+    n_2 = np.log10(get_counts(M_thresh, magnitudes[int(indices[0]) : int(indices[1])]))
+    plt.plot(M_thresh, n_2, "b.-", label="Event 2")
+
+    n_3 = np.log10(get_counts(M_thresh, magnitudes[int(indices[1]) : int(indices[2])]))
+    plt.plot(M_thresh, n_3, "g.-", label="Event 3")
+
+    n_4 = np.log10(get_counts(M_thresh, magnitudes[int(indices[2]) : int(indices[3])]))
+    plt.plot(M_thresh, n_4, "y.-", label="Event 4")
+
+    n_5 = np.log10(get_counts(M_thresh, magnitudes[int(indices[3]) : ]))
+    plt.plot(M_thresh, n_5, "c.-", label="Event 5")
 
     plt.legend()
     plt.grid()
+    plt.savefig("log(N) frequency of events at M magnitude.png")
 
-    #subplots of each curve against its linear regression
+    # subplots of each curve against its linear regression
 
     plt.figure()
-    plt.subplot(411)
-    plt.plot(M_thresh,n_1,"ro", label = "Event 1")
-    a,e,rsq,model = get_model(n_1,M_thresh)
-    plt.plot(M_thresh,model,"k--")
-    plt.title(f"Event 1: params: {a}, R^2: {rsq}", loc='left')
-    plt.xlabel("Magnitude")
+    plt.subplot(311)
+    plt.plot(M_thresh, n_1, "ro", label="Event 1")
+    a, e, rsq, model = get_model(n_1, M_thresh)
+    plt.plot(M_thresh, model, "k--")
+    plt.title(f"Event 1: params: {a}, R^2: {rsq}", loc="left")
+    plt.xlabel("magnitude")
     plt.ylabel("log(N) number of events")
 
-    plt.subplot(412)
-    plt.plot(M_thresh,n_2,"bo", label = "Event 2")
-    a,e,rsq,model = get_model(n_2,M_thresh)
-    plt.plot(M_thresh,model,"k--")
-    plt.title(f"Event 2: params: {a}, R^2: {rsq}", loc='left')
-    plt.xlabel("Magnitude")
+    plt.subplot(312)
+    plt.plot(M_thresh, n_2, "bo", label="Event 2")
+    a, e, rsq, model = get_model(n_2, M_thresh)
+    plt.plot(M_thresh, model, "k--")
+    plt.title(f"Event 2: params: {a}, R^2: {rsq}", loc="left")
+    plt.xlabel("magnitude")
+    plt.ylabel("log(N) number of events")
+
+    plt.subplot(313)
+    plt.plot(M_thresh, n_3, "go", label="Event 3")
+    a, e, rsq, model = get_model(n_3, M_thresh)
+    plt.plot(M_thresh, model, "k--")
+    plt.title(f"Event 3: params: {a}, R^2: {rsq}", loc="left")
+    plt.xlabel("magnitude")
+    plt.ylabel("log(N) number of events")
+
+    plt.savefig("linear regression subplots1.png")
+
+    plt.figure()
+    plt.subplot(211)
+    plt.plot(M_thresh, n_4, "yo", label="Event 4")
+    a, e, rsq, model = get_model(n_4, M_thresh)
+    plt.plot(M_thresh, model, "k--")
+    plt.title(f"Event 4: params: {a}, R^2: {rsq}", loc="left")
+    plt.xlabel("magnitude")
+    plt.ylabel("log(N) number of events")
+
+    plt.subplot(212)
+    plt.plot(M_thresh, n_5, "co", label="Event 5")
+    a, e, rsq, model = get_model(n_5, M_thresh)
+    plt.plot(M_thresh, model, "k--")
+    plt.title(f"Event 5: params: {a}, R^2: {rsq}", loc="left")
+    plt.xlabel("magnitude")
     plt.ylabel("log(N) number of events")
     
-    plt.subplot(413)
-    plt.plot(M_thresh,n_3,"go", label = "Event 3")
-    a,e,rsq,model = get_model(n_3,M_thresh)
-    plt.plot(M_thresh,model,"k--")
-    plt.title(f"Event 3: params: {a}, R^2: {rsq}", loc='left')
-    plt.xlabel("Magnitude")
-    plt.ylabel("log(N) number of events")
-    
-    plt.subplot(414)
-    plt.plot(M_thresh,n_4,"yo", label = "Event 4")
-    a,e,rsq,model = get_model(n_4,M_thresh)
-    plt.plot(M_thresh,model,"k--")
-    plt.title(f"Event 4: params: {a}, R^2: {rsq}", loc='left')
-    plt.xlabel("Magnitude")
-    plt.ylabel("log(N) number of events")
+    plt.savefig("linear regression subplots2.png")
 
     plt.show()
 
-def get_model(N,M):
-     
-    y = np.transpose(N)
-    Z = np.transpose(np.array([np.ones(M.shape),M])) 
-    a,e,rsq,model = multi_regress(y,Z)
 
-    return a,e,rsq,model
+def get_model(N, M):
+    y = np.transpose(N)
+    Z = np.transpose(np.array([np.ones(M.shape), M]))
+    a, e, rsq, model = multi_regress(y, Z)
+
+    return a, e, rsq, model
+
 
 def testing_data():
-    #sample data from in-class problem for ease of verification
-    #the output is correct
-    y = np.transpose(np.array([22.8,22.8,22.8,20.6,13.9,11.7,11.1,11.1]))
-    Z = np.transpose(np.array([[1,1,1,1,1,1,1,1],[0,2.3,4.9,9.1,13.7,18.3,22.9,27.2]]))
-    
-    a,e,rsq,model = multi_regress(y,Z)
-    
-    print(f"##########\nFor the test data from the example in class\nThe vector of model coefficients is {a}\nThe R^2 value is: {rsq}\n##########")
-    
+    # sample data from in-class problem for ease of verification
+    # the output is correct
+    y = np.transpose(np.array([22.8, 22.8, 22.8, 20.6, 13.9, 11.7, 11.1, 11.1]))
+    Z = np.transpose(
+        np.array([[1, 1, 1, 1, 1, 1, 1, 1], [0, 2.3, 4.9, 9.1, 13.7, 18.3, 22.9, 27.2]])
+    )
+
+    a, e, rsq, model = multi_regress(y, Z)
+
+    print(
+        f"##########\nFor the test data from the example in class\nThe vector of model coefficients is {a}\nThe R^2 value is: {rsq}\n##########"
+    )
+
     return None
 
-def get_counts(threshold,magnitude_slice):
-    
+
+def get_counts(threshold, magnitude_slice):
     M_counts = np.zeros(0)
-    
-    for index,M_thresh in enumerate(threshold):
-        M_counts = np.append(M_counts,np.sum(np.where(magnitude_slice>=threshold[index],1,0)))
+
+    for index, M_thresh in enumerate(threshold):
+        M_counts = np.append(
+            M_counts, np.sum(np.where(magnitude_slice >= threshold[index], 1, 0))
+        )
 
     return M_counts
 
 
 if __name__ == "__main__":
-        main()
+    main()
--- src/regression.py
+++ src/regression.py
@@ -1,47 +1,50 @@
 import numpy as np
 
-def multi_regress(y,Z):
+
+def multi_regress(y, Z):
     """Performs multiple linear regression.
-        
-        Parameters
-        ----------
-        y : array_like, shape = (n,) or (n,1)
-        The vector of dependent variable data
-        
-        Z : array_like, shape = (n,m)
-        The matrix of independent variable data
-        
-        Returns
-        -------
-        numpy.ndarray, shape = (m,) or (m,1)
-        The vector of model coefficients
-        
-        numpy.ndarray, shape = (n,) or (n,1)
-        The vector of residuals
-        
-        float
-        The coefficient of determination, r^2
 
-        numpy.ndarray, shape = (n,) or (n,1)
-        The vector of model outputs
-        """
-    #setup matrices to find coefficients 
-    ZTZ = np.matmul(np.transpose(Z),Z)
-    ZTy = np.matmul(np.transpose(Z),y)
-    a = np.matmul(np.linalg.inv(ZTZ),ZTy)
+    Parameters
+    ----------
+    y : array_like, shape = (n,) or (n,1)
+    The vector of dependent variable data
 
-    #calculate mean model
-    T_model_avg = np.full(y.shape,np.mean(y))
+    Z : array_like, shape = (n,m)
+    The matrix of independent variable data
+
+    Returns
+    -------
+    numpy.ndarray, shape = (m,) or (m,1)
+    The vector of model coefficients
+
+    numpy.ndarray, shape = (n,) or (n,1)
+    The vector of residuals
+
+    float
+    The coefficient of determination, r^2
+
+    numpy.ndarray, shape = (n,) or (n,1)
+    The vector of model outputs
+    """
+    # setup matrices to find coefficients
+    ZTZ = Z.T@Z
+    ZTy = Z.T@y
+    a = np.linalg.inv(ZTZ)@ZTy
+
+    # calculate mean model
+    T_model_avg = np.full(y.shape, np.mean(y))
     e_avg = y - T_model_avg
     S_t = np.sum(np.square(e_avg))
 
-    #calculate first order linear model
-    T_model_linear = np.matmul(Z,a)
+    # calculate first order linear model
+    T_model_linear = Z@a
     e_linear = y - T_model_linear
     S_r = np.sum(np.square(e_linear))
 
+    #get r^2 value
     rsq = (S_t - S_r) / S_t
-    
+
+    #residuals equal to linear model residuals
     e = e_linear
-    
+
     return a, e, rsq, T_model_linear
